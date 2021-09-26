62e7c7ca6d8341a0f20f330a8d88536005701cc9
vaibhavw12 <vaibhavwabe2000@gmail.com>
2021-09-26 23:54:32 +0530

    pakages created

--- src/com/vaibhav/Main.java
+++ src/com/vaibhav/Main.java
@@ -3,5 +3,10 @@ package com.vaibhav;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(isOdd(107));
+    }
+
+    private static int isOdd(int i) {
+        return (i & 1) ;
     }
 }
