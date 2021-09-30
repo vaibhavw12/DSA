9bc6f760ce672837f1b48670d256598b9ac011df
vaibhavw12 <vaibhavwable2000@gmail.com>
2021-09-30 18:24:49 +0530

    updation4

--- src/com/vaibhav/pepcoding/arrays/Arrays9.java
+++ src/com/vaibhav/pepcoding/arrays/Arrays9.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 //subsets of an Arrays
 public class Arrays9 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String num = in.next();
-        char[] arr;
-        arr = num.toCharArray();
-        //for n elements we have 2**n subsets
+//        Scanner in = new Scanner(System.in);
+//        String num = in.next();
+//        char[] arr;
+//        arr = num.toCharArray();
+//        //for n elements we have 2**n subsets
 
     }
 
