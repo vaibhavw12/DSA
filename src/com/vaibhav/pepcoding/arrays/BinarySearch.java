6b968bb726bc38762e56cd28e13d7c7bcbcec59a
vaibhavw12 <vaibhavwable2000@gmail.com>
2021-10-07 02:59:56 +0530

    search in 2D sorted matrix

--- src/com/vaibhav/pepcoding/arrays/BinarySearch.java
+++ src/com/vaibhav/pepcoding/arrays/BinarySearch.java
@@ -1,6 +1,6 @@
 package com.vaibhav.pepcoding.arrays;
 
-public class binarySearch {
+public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,546};
         int num = 5;
---
+++ src/com/vaibhav/pepcoding/matrix/Sreachin2DSortedMatrix.java
@@ -0,0 +1,36 @@
+package com.vaibhav.pepcoding.matrix;
+
+import com.vaibhav.pepcoding.arrays.BinarySearch;
+
+import java.util.Scanner;
+
+public class Sreachin2DSortedMatrix {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("enter rows and columns for matrix :");
+        int r = in.nextInt();
+        int c = in.nextInt();                                                 //  1   2   3   4
+        int[][] mat = new int[r][c];                                          //  5   6   7   8     saddle point -> 13
+        System.out.println("enter elements row wise :");                      //  9   10  11  12
+        matrixMult.createMatrix(mat,r,c);                                     //  13  14  15  16
+        System.out.println("enter number to searched :");
+        int num = in.nextInt();
+        searchin2DSortedMatrix(mat,r,c,num);
+    }
+
+    private static void searchin2DSortedMatrix(int[][] mat, int r, int c, int num) {
+        c--;
+        for(int i=0;i<r;i++){
+            if(mat[i][c]==num){
+                System.out.println(i+","+c);
+                return;
+            }else if(mat[i][c]>num){
+                int a =BinarySearch.binary(mat[i],num);
+                System.out.println(i+","+a);
+                return;
+            }else{}
+        }
+        System.out.println("not found");
+    }
+}
+
